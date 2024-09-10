32b3caa91244e21208c509f8443b60d2dce17050
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-09-10 08:13:30 -0500

    se crea el controlador de mercancia

---
+++ src/main/java/com/example/BODEGASTCCAPI/helpers/mensajes/Mensaje.java
@@ -0,0 +1,28 @@
+package com.example.BODEGASTCCAPI.helpers.mensajes;
+
+public enum Mensaje {
+
+    PESO_NEGATIVO("El peso no puede ser negativo"),
+
+    VOLUMEN_NEGATIVO("El volumen no puede ser negativo"),
+
+    FECHA_INVALIDA("Revisa la fecha ingresada")
+
+
+
+    ;
+
+    private String mensaje;
+
+    Mensaje() {
+    }
+
+    Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+}
