d53f2ee572edc2781b943d5665370bd64d9b25a1
davidssm97 <dssalazar@tcc.com.co>
2024-09-24 18:23:58 -0500

    se añade la carpeta con las capturas en la carpeta resources y se modifica MercanciaValidacion para que use correctamente la validacion de validarNombre

--- src/main/java/com/example/BODEGASTCCAPI/servicios/MercanciaServicio.java
+++ src/main/java/com/example/BODEGASTCCAPI/servicios/MercanciaServicio.java
@@ -27,30 +27,34 @@ public class MercanciaServicio {
     MercanciaValidacion validacion;
 
     //guardar
-    public Mercancia almacenarMercancia(Mercancia datosMercancia) throws Exception{
-        try{
-
-            //aplicar validaciones a los datos recibidos
-            //si sale bien la validacion llamo al repo para guardar los datos
-            if(!this.validacion.validarPeso(datosMercancia.getPeso())){
+    //guardar
+    public Mercancia almacenarMercancia(Mercancia datosMercancia) throws Exception {
+        try {
+            // aplicar validaciones a los datos recibidos
+            if (!this.validacion.validarPeso(datosMercancia.getPeso())) {
                 throw new Exception(Mensaje.PESO_NEGATIVO.getMensaje());
             }
 
-            if(!this.validacion.validarVolumen(datosMercancia.getVolumen())){
+            if (!this.validacion.validarVolumen(datosMercancia.getVolumen())) {
                 throw new Exception(Mensaje.VOLUMEN_NEGATIVO.getMensaje());
-
             }
 
-            if(!this.validacion.validarFechas(datosMercancia.getFechaIngreso(), LocalDate.now())){
+            if (!this.validacion.validarFechas(datosMercancia.getFechaIngreso(), LocalDate.now())) {
                 throw new Exception(Mensaje.FECHA_INVALIDA.getMensaje());
+            }
+
+            // Validar el nombre de la mercancía
+            if (!this.validacion.validarNombre(datosMercancia.getNombre())) {
+                throw new Exception(Mensaje.NOMBRE_INVALIDO.getMensaje()); // Suponiendo que tienes un mensaje definido para nombre inválido
             }
 
             return this.repositorio.save(datosMercancia);
 
-        }catch(Exception error){
+        } catch (Exception error) {
             throw new Exception(error.getMessage());
         }
     }
+
 
     //buscar todos
     public List<Mercancia> buscarTodasMercancias(){
